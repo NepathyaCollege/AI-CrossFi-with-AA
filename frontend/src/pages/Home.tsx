f7d57401f5884f8e036cd08d08b5ccd5e6633199
Sagar Thapa Chhetri <111417232+R3yz0n@users.noreply.github.com>
2024-08-05 15:11:15 +0545

    Config/tailwind setup (#4)
    * removed couter
    
    * feat:configure tailwindcss
    
    * add: routing in App component
    
    * fix: remove unused files & create Homepage
    
    ---------
    
    Co-authored-by: nirajbhattarai <nirajbhattarai19@gmail.com>
    Co-authored-by: Niraj Bhattarai <39485271+NirajBhattarai@users.noreply.github.com>

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,25 +1,7 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import React from "react";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+  return <div>Home</div>;
 };
 
 export default Home;
