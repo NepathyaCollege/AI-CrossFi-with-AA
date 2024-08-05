5809c7e910af512922f996fe23a7402fe77bf644
r3yz0n <reyzon.thapa2020@gmail.com>
2024-08-05 17:37:25 +0545

    chore: Update API URLs in frontend config file

--- frontend/src/config/apiUrl.ts
+++ frontend/src/config/apiUrl.ts
@@ -1,11 +1,11 @@
 export const CHECK_EMAIL_URL =
-  "https://9uxdnapxfa.execute-api.us-east-1.amazonaws.com/dev/checkEmail";
+  "https://0tvur9zmhh.execute-api.us-east-1.amazonaws.com/dev/checkEmail";
 
-export const SIGNUP_URL = "https://9uxdnapxfa.execute-api.us-east-1.amazonaws.com/dev/signup";
+export const SIGNUP_URL = "https://0tvur9zmhh.execute-api.us-east-1.amazonaws.com/dev/signup";
 export const EMAIL_VERIFICATION_URL =
-  "https://9uxdnapxfa.execute-api.us-east-1.amazonaws.com/dev/signup";
+  "https://0tvur9zmhh.execute-api.us-east-1.amazonaws.com/dev/signup";
 
-export const VERIFY_OTP = "https://9uxdnapxfa.execute-api.us-east-1.amazonaws.com/dev/verifyOTP";
+export const VERIFY_OTP = "https://0tvur9zmhh.execute-api.us-east-1.amazonaws.com/dev/verifyOTP";
 
 export const CHECK_PASSWORD_URL =
-  "https://9uxdnapxfa.execute-api.us-east-1.amazonaws.com/dev/checkPassword";
+  "https://0tvur9zmhh.execute-api.us-east-1.amazonaws.com/dev/checkPassword";
