1c8e9fccedb2265ddaeb986bcf92a97b2a399fe0
r3yz0n <reyzon.thapa2020@gmail.com>
2024-08-05 15:09:18 +0545

    add: routing in App component

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import { IonApp, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-const container = document.getElementById('root');
+import "./theme/tailwind.css";
+import "./theme/variables.css";
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+import App from "./App";
+
+setupIonicReact();
+const container = document.getElementById("root");
 const root = createRoot(container!);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <React.Fragment>
+    <IonApp className="h-screen w-screen ">
+      <IonReactRouter>
+        <App />
+      </IonReactRouter>
+    </IonApp>
+  </React.Fragment>
 );
