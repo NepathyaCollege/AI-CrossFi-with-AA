ccd379527122375cbd2b19a2aa841ee4e9cf9147
nirajbhattarai <nirajbhattarai19@gmail.com>
2024-08-09 16:07:52 +0545

    made minor changes on deployment script

---
+++ contracts/script/DeployMultiTokenKeeperFactory.s.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+// import "../src/chainlink/NepathyaPool.sol";
+import "../src/chainlink/limitorderkeeper/MultiTokenKeeperFactory.sol";
+import "../src/chainlink/limitorderkeeper/MultiTokenKeeper.sol";
+
+import "../src/helpers/Token.sol";
+
+contract DeployMultiTokenKeeperFactory is Script {
+    function run() public {
+        // Load environment variables
+        address routerAddress = 0x1689E7B1F10000AE47eBfE339a4f69dECd19F602;
+        address linkTokenAddress = 0xE4aB69C077896252FAFBD49EFD26B5D171A32410;
+        address tokenAddress = 0x84cEc3F89A0f28803F177575BbF7b2010A62d2Ec;
+
+        address usdt = 0xe7A527BD98566FDc99EA72bf16c6cc4eFe3606a0;
+        address aggregatorManager = 0xF7d7F1882d30B3A8d39A3DB46600935096aC5d50;
+        address keeperRegistryAddress = 0x80C55e674a34FfE730B0357E16e8852B19573f7C;
+        uint256 linkAmount = 5 ether;
+
+        // Start broadcasting the transaction
+        vm.startBroadcast();
+
+        MultiTokenKeeperFactory multiTokenKeeperFactory = new MultiTokenKeeperFactory(
+            routerAddress, usdt, aggregatorManager, keeperRegistryAddress, linkTokenAddress, linkAmount
+        );
+        Token(linkTokenAddress).approve(address(multiTokenKeeperFactory), 10000 ether);
+
+        multiTokenKeeperFactory.createAndRegisterMultiTokenKeeper();
+
+        address keeper = multiTokenKeeperFactory.getMultiTokenKeeper(msg.sender);
+
+             // Mint tokens to the MultiTokenKeeper contract
+        Token tokenA = Token(usdt);
+        tokenA.mint(address(keeper), 10000 ether);
+
+        MultiTokenKeeper(keeper).addOrder(
+            0x0f5C50184EF3ADF2581fd115A0eB4f3cC7F296A1,
+            0x0FB99723Aee6f420beAD13e6bBB79b7E6F034298,
+            OrderManager.OrderType.Buy,
+            0,
+            100 ether
+        );
+
+        // Deploy the contract
+        // nepathyaPool = NepathyaPool(0x20Fa0Fc715121F50ffe229E0DB1504543d04cEA3); //new NepathyaPool(routerAddress, linkTokenAddress, tokenAddress);
+        // // IERC20(linkTokenAddress).transfer(address(nepathyaPool), 1000000000000000000);
+
+        // // IERC20(tokenAddress).approve(address(nepathyaPool), 100000000000000000000000000000000000000);
+        // // Token(tokenAddress).mint(0x75998e806D0BE5B37c5DE74AcfA0006B3C7DCdfF, 100000000000000000000000000000000000000);
+
+        // SendParam memory sendParam = SendParam({
+        //     to: 0x75998e806D0BE5B37c5DE74AcfA0006B3C7DCdfF, // Replace with actual recipient address
+        //     amount: 1000000000 // Replace with actual amount
+        // });
+
+        // nepathyaPool.sendMessagePayLINK(16015286601757825753, 0x82edE4BE52222D2EB93E9A23D6fc94645fe06Cc5, sendParam);
+
+        // Stop broadcasting the transaction
+        vm.stopBroadcast();
+    }
+}
---
+++ contracts/src/chainlink/limitorderkeeper/MultiTokenKeeper.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@chainlink/contracts/src/v0.8/AutomationCompatible.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
+import {IERC20} from
+    "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.3/contracts/token/ERC20/IERC20.sol";
+import "./AggregatorManager.sol";
+import "./OrderManager.sol";
+
+contract MultiTokenKeeper is AutomationCompatibleInterface {
+    AggregatorManager public aggregatorManager;
+    OrderManager public orderManager;
+
+    address public usdtAddress;
+    IUniswapV2Router02 public uniswapRouter;
+
+    constructor(address _uniswapRouter, address _usdtAddress, address _aggregatorManager) {
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
+        usdtAddress = _usdtAddress;
+        aggregatorManager = AggregatorManager(_aggregatorManager);
+        orderManager = new OrderManager();
+    }
+
+    event Price(int256 price);
+    event TokenPurchased(address token, uint256 amount);
+    event TokenSold(address token, uint256 amount);
+
+    function addOrder(
+        address _token,
+        address _priceFeed,
+        OrderManager.OrderType _orderType,
+        int256 _priceThreshold,
+        uint256 _amount
+    ) external {
+        require(aggregatorManager.isAggregatorApproved(_priceFeed), "Aggregator not approved");
+
+        orderManager.addOrder(_token, _priceFeed, _orderType, _priceThreshold, _amount);
+    }
+
+    function getLatestPrice(address priceFeed) public view returns (int256) {
+        (, int256 price,,,) = AggregatorV3Interface(priceFeed).latestRoundData();
+        return price;
+    }
+
+    function checkUpkeep(bytes calldata checkData)
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory performData)
+    {
+        upkeepNeeded = false;
+        OrderManager.Order[] memory activeOrders = orderManager.getActiveOrders();
+        for (uint256 i = 0; i < activeOrders.length; i++) {
+            int256 latestPrice = getLatestPrice(activeOrders[i].priceFeed);
+            // if (
+            //     // (
+            //         activeOrders[i].orderType == OrderManager.OrderType.Buy
+            //             // && latestPrice < activeOrders[i].priceThreshold
+            //     // )
+            //         // || (
+            //         //     activeOrders[i].orderType == OrderManager.OrderType.Sell
+            //         //         && latestPrice > activeOrders[i].priceThreshold
+            //         // )
+            // ) {
+            upkeepNeeded = true;
+            performData = abi.encode(activeOrders[i]);
+            break;
+            // }
+        }
+    }
+
+    function performUpkeep(bytes calldata performData) external override {
+        OrderManager.Order memory order = abi.decode(performData, (OrderManager.Order));
+        int256 latestPrice = getLatestPrice(order.priceFeed);
+
+        emit Price(latestPrice);
+        if (order.orderType == OrderManager.OrderType.Buy) {
+            buyToken(order);
+        } else if (order.orderType == OrderManager.OrderType.Sell) {
+            sellToken(order);
+        }
+
+        orderManager.markOrderAsProcessed(order.id);
+    }
+
+    function buyToken(OrderManager.Order memory order) internal {
+        address token = order.token;
+        uint256 amount = order.amount;
+
+        address[] memory pair = new address[](2);
+        pair[0] = usdtAddress;
+        pair[1] = token;
+
+        IERC20(usdtAddress).approve(address(uniswapRouter), amount);
+
+        uniswapRouter.swapExactTokensForTokens(
+            amount,
+            0, // Accept any amount of token
+            pair,
+            address(this),
+            block.timestamp + 15
+        );
+
+        emit TokenPurchased(token, amount);
+    }
+
+    function sellToken(OrderManager.Order memory order) internal {
+        address token = order.token;
+        uint256 amount = order.amount;
+
+        address[] memory pair = new address[](2);
+        pair[0] = token;
+        pair[1] = usdtAddress;
+
+        IERC20(token).approve(address(uniswapRouter), amount);
+
+        uniswapRouter.swapExactTokensForTokens(
+            amount,
+            0, // Accept any amount of USDT
+            pair,
+            address(this),
+            block.timestamp + 15
+        );
+
+        emit TokenSold(token, amount);
+    }
+}
