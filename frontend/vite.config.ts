8aaff0e81ac2d0c2117e89c2c0cb25133b5efbe5
nirajbhattarai <nirajbhattarai19@gmail.com>
2024-08-11 13:44:36 +0545

    fixed missed conflict

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-<<<<<<< HEAD
     target: "es2020",
   },
   optimizeDeps: {
@@ -12,30 +11,3 @@ export default defineConfig({
     },
   },
 });
-=======
-    target: 'es2020', // you can also use 'es2020' here
-    chunkSizeWarningLimit: 1000, // Increase limit to 1000 kB
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      target: "esnext", 
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis'
-      },
-      supported: { 
-        bigint: true 
-      },
-    }
-  },
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
->>>>>>> 8646867 (wip)
